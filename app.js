f722f273cda1fb1c01e62581be65d346f1c2c1e1
unknown <nurs.axe@gmail.com>
2022-10-23 16:05:42 +0600

    edit js file

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const cols = document.querySelectorAll('.col')
 
-document.addEventListener('keydown', event => {
-    event.preventDefault();
+document.addEventListener('keydown', (event) => {
+    event.preventDefault()
     if (event.code.toLowerCase() === 'space') {
         setRandomColors()
     }
 })
 
-document.addEventListener('click', event => {
+document.addEventListener('click', (event) => {
     const type = event.target.dataset.type
 
     if (type === 'lock') {
@@ -15,75 +15,64 @@ document.addEventListener('click', event => {
             ? event.target
             : event.target.children[0]
 
-        node.classList.toggle('fa-lock-open');
-        node.classList.toggle('fa-lock');
-    } else if(type === 'copy') {
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    } else if (type === 'color') {
         copyToClipBoard(event.target.textContent)
-        alert('text coping')
+        alert('Цвет скопирован')
     }
 })
 
-function genereteRandomColor() {
-
-    const hexCodes = "0123456789ABCDEF"
-    let color = ""
-    for (let i = 0; i < 6; i++) {
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-    }
-
-    return '#' + color
-}
-
-function copyToClipBoard(text) {
+const copyToClipBoard = (text) => {
     return navigator.clipboard.writeText(text)
 }
 
-function setRandomColors(isInitial) {
+const setRandomColors = (isInitial) => {
     const colors = isInitial ? getColorsFromHash() : []
 
     cols.forEach((col, index) => {
-        const isLocked = col.querySelector('i').classList.contains('fa-lock');
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
         const button = col.querySelector('button')
-        
-        if(isLocked) {
+
+        if (isLocked) {
             colors.push(text.textContent)
             return
         }
-        
+
         const color = isInitial 
             ? colors[index] 
                 ? colors[index]
                 : chroma.random()
             : chroma.random()
-
+        
         if (!isInitial) {
             colors.push(color)
         }
-
+        
         text.textContent = color
         col.style.background = color
 
-        setTextColor(text, color)
+        setTextColor(text, color) 
         setTextColor(button, color)
     })
 
     updateColorHash(colors)
 }
 
-function setTextColor(text, color) {
+const setTextColor = (text, color) => {
     const luminance = chroma(color).luminance()
-    text.style.color = luminance > 0.5 ? 'black' : 'white';
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
-function updateColorHash(colors = []) {
+const updateColorHash = (colors = []) => {
     document.location.hash = colors.map(col => {
         return col.toString().substring(1)
     }).join('-')
 }
 
-function getColorsFromHash() {
-    if(document.location.hash.length > 1) {
+const getColorsFromHash = () => {
+    if (document.location.hash.length > 1) {
         return document.location.hash
             .substring(1)
             .split('-')
